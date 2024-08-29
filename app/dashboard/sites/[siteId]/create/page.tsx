845173c6486974c1c7c7089f7ef721d5e7c70937
assarjunior <theomarassar@gmail.com>
2024-08-29 19:39:22 +0300

    Upload images in sites working

--- app/dashboard/sites/[siteId]/create/page.tsx
+++ app/dashboard/sites/[siteId]/create/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { UploadDropzone } from "@/app/utils/UploadthingComponents";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,13 +13,17 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { ArrowLeft, Atom } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function ArticleCreationRoute({
   params,
 }: {
   params: { siteId: string };
 }) {
+  const [imageUrl, setImageUrl] = useState<undefined | string>(undefined);
   return (
     <>
       <div className="flex items-center">
@@ -57,6 +64,30 @@ export default function ArticleCreationRoute({
                 placeholder="Small Description for your blog article ...."
                 className="h-32"
               />
+            </div>
+
+            <div className="grid gap-2">
+              <Label>Cover Image</Label>
+              {imageUrl ? (
+                <Image
+                  src={imageUrl}
+                  width={200}
+                  height={200}
+                  alt="uploaded image w-[200px] h-[200px] rounded-lg"
+                  className="object-cover"
+                />
+              ) : (
+                <UploadDropzone
+                  onClientUploadComplete={(res) => {
+                    setImageUrl(res[0].url);
+                    toast.success("Image has been uploaded")
+                  }}
+                  endpoint="imageUploader"
+                  onUploadError={() => {
+                    toast.error("Something Went wrong")
+                  }}
+                />
+              )}
             </div>
           </form>
         </CardContent>
