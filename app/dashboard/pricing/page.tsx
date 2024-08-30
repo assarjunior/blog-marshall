8c22f971a65b387212e278defaa7e8f8977c7ccf
assarjunior <theomarassar@gmail.com>
2024-08-30 05:51:07 +0300

    Pricing page done

---
+++ app/dashboard/pricing/page.tsx
@@ -0,0 +1,9 @@
+import { PricingTable } from "@/app/components/shared/Pricing";
+
+export default function PricingList() {
+    return (
+        <div>
+            <PricingTable />
+        </div>
+    )
+}
